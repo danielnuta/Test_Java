1c3fd41769eae0a6239982dbbb78b63fea553490
daniel.nuta <danielnuta91@gmail.com>
2024-08-25 23:16:54 +0300

    playwright one

---
+++ src/main/java/org/example/Fish.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Fish {
+    public static void main(String[] args) {
+        int numFish = 4;
+        String fishType = "tuna";
+        int anotherFish = numFish + 1;
+        System.out.println(anotherFish + " " + fishType);
+        System.out.println((numFish + 1) + " " + fishType);
+    }
+}
