109f37e815fe9c4811080b58a334f77591b5c0e6
daniel.nuta <danielnuta91@gmail.com>
2024-08-26 22:49:02 +0300

    playwright one

--- src/main/java/org/example/Profit.java
+++ src/main/java/org/example/Profit.java
@@ -1,13 +1,13 @@
-package org.example;
-
-public class Profit {
-    public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                maxProfit += prices[i] - prices[i - 1];
-            }
-        }
-        return maxProfit;
-    }
-}
+//package org.example;
+//
+//public class Profit {
+//    public int maxProfit(int[] prices) {
+//        int maxProfit = 0;
+//        for (int i = 1; i < prices.length; i++) {
+//            if (prices[i] > prices[i - 1]) {
+//                maxProfit += prices[i] - prices[i - 1];
+//            }
+//        }
+//        return maxProfit;
+//    }
+//}
